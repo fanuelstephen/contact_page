983b767a8cae82fce29d9ff51d06f0afde26e0e7
fanuelstephen <115701037+fanuelstephen@users.noreply.github.com>
2024-04-28 00:29:46 +0300

    add some changes in media queries

--- src/componets/Sidebar.jsx
+++ src/componets/Sidebar.jsx
@@ -1,36 +1,13 @@
 // Sidebar Component
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import {
-  MdKeyboardDoubleArrowLeft,
-  MdOutlineKeyboardDoubleArrowRight,
-} from "react-icons/md";
 import { contacts } from "../data/contactData";
 
 function Sidebar() {
-  const [hideIcon, setHideIcon] = useState(false);
-
-  const handleClick = () => {
-    setHideIcon(!hideIcon);
-  };
-
   return (
     <React.Fragment>
-      <section className={`sidebar ${hideIcon ? "hide" : "show"}`}>
+      <section className="sidebar">
         <div className="sidebar-search">
-          <div className="siderbar-icon">
-            {hideIcon ? (
-              <MdOutlineKeyboardDoubleArrowRight
-                className="icon"
-                onClick={handleClick}
-              />
-            ) : (
-              <MdKeyboardDoubleArrowLeft
-                className="icon"
-                onClick={handleClick}
-              />
-            )}
-          </div>
           <div className="search">
             <input placeholder="Search..." type="text" />
             <button type="submit">Go</button>
